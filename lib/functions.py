6553405a7fdb85d80a647571adb939921a575df8
Ashley King <Princessak8918@hotmail.com>
2023-05-01 20:46:51 -0600

    pass test

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python3
 
-def greet_programmer():
-    pass
+def greet_programmer ():
+    print("Hello, programmer!")    
+    
 
-def greet(name):
-    pass
+def greet(Naureen):
+    print (f"Hello, {Naureen}!")
+    
 
 def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
     pass
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+    
 
 def halve(number):
+    if type (number) == int:
+        return number / 2
+    elif type(number) == float:
+        return number / 2.0 
+    else:
+        return None
     pass
+        
+        
+    
